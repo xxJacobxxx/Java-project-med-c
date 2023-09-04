00cd444e784161c503f4356ad7ea31d78487aec7
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-04 09:14:49 +0200

    Lad till kvot och produkt

--- src/App.java
+++ src/App.java
@@ -6,6 +6,11 @@ public class App {
         int differens = tal1-tal2;
         System.out.println("summan av kardemumman är "+ summa);
         System.out.println("differensen av kardemumman är " +differens);
+        int produkt=tal1*tal2;
+        int kvot = tal1/tal2;
+        System.out.println("produkten av kardemumman är "+ produkt);
+        System.out.println("kvoten av kardemumman är "+ kvot);
+        
 
     }
 }
