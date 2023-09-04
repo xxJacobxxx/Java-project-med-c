8e028372fc26507d960d9fc8b7cc08582303edc7
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-04 09:22:53 +0200

    Lagt till moduler och doubles

--- src/App.java
+++ src/App.java
@@ -10,7 +10,22 @@ public class App {
         int kvot = tal1/tal2;
         System.out.println("produkten av kardemumman är "+ produkt);
         System.out.println("kvoten av kardemumman är "+ kvot);
-        
+        int vila1 = 10%3;
+        int vila2 = 9%3;
+        System.out.println("Resten av kardemumman är "+ vila1+ "och "+ vila2);
+        double dectal1 = 5.0;
+        double dectal2 = 23.41;
+        double add = dectal1+dectal2;
+        double diff = dectal1-dectal2;
+        double prod = dectal1*dectal2;
+        double qoute = dectal1/dectal2;
+        System.out.println("Jag orkar inte längre "+ +add);
+        System.out.println("Jag orkar inte längre "+ +diff);
+        System.out.println("Jag orkar inte längre "+ +prod);
+        System.out.println("Jag orkar inte längre "+ +qoute);
+
+
+
 
     }
 }
