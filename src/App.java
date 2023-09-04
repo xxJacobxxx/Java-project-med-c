fa7f5e2123a416efcef7ed1121cf01c876f8aafd
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-04 09:10:11 +0200

    La till addition och subtraktion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 2;
+        int tal2 = 30;
+        int summa = tal1+tal2;
+        int differens = tal1-tal2;
+        System.out.println("summan av kardemumman är "+ summa);
+        System.out.println("differensen av kardemumman är " +differens);
+
     }
 }
